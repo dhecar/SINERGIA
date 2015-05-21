8e301119bb914fef5c388e2bf657fa53d1310b97
dhecar <dhecar@gmail.com>
2015-05-21 18:17:55 +0200

    Changed description

--- sine_stock_priority/__openerp__.py
+++ sine_stock_priority/__openerp__.py
@@ -1,13 +1,15 @@
 # -*- coding: utf-8 -*-
 {
-    "name" : "Stock warehouse priority",
-    "version" : "7.0",
-    "author" : "David Hernández",
-    "category" : "Generic Modules/Others",
-    "website" : "http://sinergiainformatica.net",
-    "description": "Extension",
-    "depends" : ["product","stock_location","stock"],
-    "update_xml" : [],
+    "name": "Stock warehouse priority",
+    "version": "7.0",
+    "author": "David Hernández",
+    "category": "Generic Modules/Others",
+    "website": "http://sinergiainformatica.net",
+    "description": "Extension que añade una validación en los envios internos si el stock de origen "
+                   "se queda por debajo de 1 en el momento de validar el envio, dando prioridad de stock"
+                   "al almacén local",
+    "depends": ["product", "stock_location", "stock"],
+    "update_xml": [],
     "data": ['stock_priority_wfl.xml', 'security/security.xml', 'security/ir.model.access.csv'],
     "active": True,
     "installable": True
