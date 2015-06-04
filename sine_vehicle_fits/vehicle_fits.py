11d9d9abf4b4f55ef5f580f454a9c8582a5fa99d
dhecar <dhecar@gmail.com>
2015-06-04 12:31:46 +0200

    coding utf8

--- sine_vehicle_fits/vehicle_fits.py
+++ sine_vehicle_fits/vehicle_fits.py
@@ -25,9 +25,7 @@ import csv
 import paramiko
 import openerp.tools as tools
 import base64
-import codecs
 from StringIO import StringIO
-
 
 
 class vehicle_config(osv.osv):
@@ -88,36 +86,6 @@ class vehicle_config(osv.osv):
 vehicle_config()
 
 
-class UnicodeWriter:
-    """
-    A CSV writer which will write rows to CSV file "f",
-    which is encoded in the given encoding.
-    """
-
-    def __init__(self, f, dialect=csv.excel, encoding="utf-8", **kwds):
-        # Redirect output to a queue
-        self.queue = cStringIO.StringIO()
-        self.writer = csv.writer(self.queue, dialect=dialect, **kwds)
-        self.stream = f
-        self.encoder = codecs.getincrementalencoder(encoding)()
-
-    def writerow(self, row):
-        self.writer.writerow([s.encode("utf-8") for s in row])
-        # Fetch UTF-8 output from the queue ...
-        data = self.queue.getvalue()
-        data = data.decode("utf-8")
-        # ... and reencode it into the target encoding
-        data = self.encoder.encode(data)
-        # write to the target stream
-        self.stream.write(data)
-        # empty queue
-        self.queue.truncate(0)
-
-    def writerows(self, rows):
-        for row in rows:
-            self.writerow(row)
-
-
 class VehicleExport(osv.osv):
     def export_to_magento(self, cr, uid, ids, context=None):
         conf_obj = self.pool.get('vehicle.config').browse(cr, uid, uid)
@@ -163,8 +131,8 @@ class VehicleExport(osv.osv):
         with open('/opt/fitments/models-to-update.csv', 'w') as f:
             writer = csv.writer(f, delimiter=',')
             writer.writerow(('sku', 'make', 'model', 'year'))
-            for row in records:
-                writer.UnicodeWriter(row)
+            for row in records.encode('utf-8'):
+                writer.writerow(row)
         conn.close()
 
         conf_line = self.pool.get('vehicle.config').browse(cr, uid, uid)
