202c486a19d38e15a43539a2a93d33de1facafbc
dhecar <dhecar@gmail.com>
2015-07-07 19:55:18 +0200

    Deleted empty lines

--- sine_brand_discount/brand_discount.py
+++ sine_brand_discount/brand_discount.py
@@ -4,17 +4,15 @@ from openerp.tools.translate import _
 from openerp.osv import fields, osv
 
 
-
-
-
 class product_brand(osv.osv):
-    
-    _name ="product.brand"
+    _name = "product.brand"
     _inherit = 'product.brand'
     _description = 'Discount gived by Manufacturer'
     _columns = {
-        'brand_discount': fields.float('Discount', digits_compute=dp.get_precision('Discount offered by the manufacturer')),
-}
+        'brand_discount': fields.float('Discount',
+                                       digits_compute=dp.get_precision('Discount offered by the manufacturer')),
+    }
+
 
 product_brand()
 
