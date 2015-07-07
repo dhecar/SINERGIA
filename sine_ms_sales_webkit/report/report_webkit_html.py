e45fe568af13e75d766dac50ab752b43d0ba88a7
dhecar <dhecar@gmail.com>
2015-07-07 19:57:55 +0200

    Deleted empty lines

--- sine_ms_sales_webkit/report/report_webkit_html.py
+++ sine_ms_sales_webkit/report/report_webkit_html.py
@@ -2,16 +2,18 @@ import time
 from report import report_sxw
 from osv import osv
 
+
 class report_webkit_html(report_sxw.rml_parse):
     def __init__(self, cr, uid, name, context):
         super(report_webkit_html, self).__init__(cr, uid, name, context=context)
         self.localcontext.update({
             'time': time,
-            'cr':cr,
+            'cr': cr,
             'uid': uid,
         })
-        
+
+
 report_sxw.report_sxw('report.sale.order.motoscoot.webkit',
-                       'sale.order',
-                       '/opt/odoo/custom/addons/SINERGIA/sine_ms_sales_webkit/report/sale_order.mako',
-                       parser=report_webkit_html)
+                      'sale.order',
+                      '/opt/odoo/custom/addons/SINERGIA/sine_ms_sales_webkit/report/sale_order.mako',
+                      parser=report_webkit_html)
