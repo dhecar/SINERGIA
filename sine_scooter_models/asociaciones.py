3834f34e38cc8d53cf5b2f97af978df0dfc37ebd
dhecar <dhecar@gmail.com>
2015-07-07 20:37:49 +0200

    Changed Author

--- sine_scooter_models/asociaciones.py
+++ sine_scooter_models/asociaciones.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
+# Sinergiainformatica.net
+# David Hernández. 2015
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
 #    published by the Free Software Foundation, either version 3 of the
