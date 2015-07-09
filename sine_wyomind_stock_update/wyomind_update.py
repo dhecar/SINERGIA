0435bd958937242e42e5a01cef4987704a41236c
dhecar <dhecar@gmail.com>
2015-07-09 14:37:55 +0200

    Add update stock for internal movements

--- sine_wyomind_stock_update/wyomind_update.py
+++ sine_wyomind_stock_update/wyomind_update.py
@@ -272,3 +272,193 @@ class stock_change_product_qty(osv.osv_memory):
 
 
 stock_change_product_qty()
+
+
+class stock_partial_picking(osv.osv_memory):
+    _inherit = "stock.partial.picking"
+
+    def do_partial(self, cr, uid, ids, context=None):
+        if context is None:
+            context = {}
+        assert len(ids) == 1, 'Partial picking processing may only be done one at a time.'
+        stock_picking = self.pool.get('stock.picking')
+        stock_move = self.pool.get('stock.move')
+        uom_obj = self.pool.get('product.uom')
+        partial = self.browse(cr, uid, ids[0], context=context)
+        partial_data = {
+            'delivery_date': partial.date
+        }
+        picking_type = partial.picking_id.type
+        for wizard_line in partial.move_ids:
+            line_uom = wizard_line.product_uom
+            move_id = wizard_line.move_id.id
+
+            # Quantiny must be Positive
+            if wizard_line.quantity < 0:
+                raise osv.except_osv(_('Warning!'), _('Please provide proper Quantity.'))
+
+            # Compute the quantity for respective wizard_line in the line uom (this jsut do the rounding if necessary)
+            qty_in_line_uom = uom_obj._compute_qty(cr, uid, line_uom.id, wizard_line.quantity, line_uom.id)
+
+            if line_uom.factor and line_uom.factor <> 0:
+                if float_compare(qty_in_line_uom, wizard_line.quantity, precision_rounding=line_uom.rounding) != 0:
+                    raise osv.except_osv(_('Warning!'), _(
+                        'The unit of measure rounding does not allow you to ship "%s %s", only rounding of "%s %s" is accepted by the Unit of Measure.') % (
+                                             wizard_line.quantity, line_uom.name, line_uom.rounding, line_uom.name))
+            if move_id:
+                # Check rounding Quantity.ex.
+                # picking: 1kg, uom kg rounding = 0.01 (rounding to 10g),
+                # partial delivery: 253g
+                # => result= refused, as the qty left on picking would be 0.747kg and only 0.75 is accepted by the uom.
+                initial_uom = wizard_line.move_id.product_uom
+                # Compute the quantity for respective wizard_line in the initial uom
+                qty_in_initial_uom = uom_obj._compute_qty(cr, uid, line_uom.id, wizard_line.quantity, initial_uom.id)
+                without_rounding_qty = (wizard_line.quantity / line_uom.factor) * initial_uom.factor
+                if float_compare(qty_in_initial_uom, without_rounding_qty,
+                                 precision_rounding=initial_uom.rounding) != 0:
+                    raise osv.except_osv(_('Warning!'), _(
+                        'The rounding of the initial uom does not allow you to ship "%s %s", as it would let a quantity of "%s %s" to ship and only rounding of "%s %s" is accepted by the uom.') % (
+                                             wizard_line.quantity, line_uom.name,
+                                             wizard_line.move_id.product_qty - without_rounding_qty, initial_uom.name,
+                                             initial_uom.rounding, initial_uom.name))
+            else:
+                seq_obj_name = 'stock.picking.' + picking_type
+                move_id = stock_move.create(cr, uid, {'name': self.pool.get('ir.sequence').get(cr, uid, seq_obj_name),
+                                                      'product_id': wizard_line.product_id.id,
+                                                      'product_qty': wizard_line.quantity,
+                                                      'product_uom': wizard_line.product_uom.id,
+                                                      'prodlot_id': wizard_line.prodlot_id.id,
+                                                      'location_id': wizard_line.location_id.id,
+                                                      'location_dest_id': wizard_line.location_dest_id.id,
+                                                      'picking_id': partial.picking_id.id
+                                                      }, context=context)
+                stock_move.action_confirm(cr, uid, [move_id], context)
+            partial_data['move%s' % (move_id)] = {
+                'product_id': wizard_line.product_id.id,
+                'product_qty': wizard_line.quantity,
+                'product_uom': wizard_line.product_uom.id,
+                'prodlot_id': wizard_line.prodlot_id.id,
+            }
+            if (picking_type == 'in') and (wizard_line.product_id.cost_method == 'average'):
+                partial_data['move%s' % (wizard_line.move_id.id)].update(product_price=wizard_line.cost,
+                                                                         product_currency=wizard_line.currency.id)
+        # Wyomind stock update
+
+        def get_stock_origin(self, cr, uid, ids, context=None):
+            stock_prod_obj = self.pool.get('stock.report.prodlots')
+            if partial.picking_id.type == 'internal' and wizard_line.location_id.usage == 'internal':
+
+                result = {}
+                stock_prod_ids = stock_prod_obj.search(cr, uid, [('product_id', '=', wizard_line.product_id.id),
+                                                                 ('location_id', '=', wizard_line.location_id.id)],
+                                                       context=context)
+
+                if stock_prod_ids:
+                    for i in stock_prod_obj.browse(cr, uid, stock_prod_ids, context=context):
+                        result = i.qty
+
+                    return result
+
+        def get_stock_dest(self, cr, uid, ids, context=None):
+            stock_prod_obj = self.pool.get('stock.report.prodlots')
+            if partial.picking_id.type == 'internal' and wizard_line.location_dest_id.usage == 'internal':
+
+                result = {}
+                stock_prod_ids = stock_prod_obj.search(cr, uid, [('product_id', '=', wizard_line.product_id.id),
+                                                                 ('location_id', '=',
+                                                                  wizard_line.location_dest_id.id)],
+                                                       context=context)
+
+                if stock_prod_ids:
+                    for i in stock_prod_obj.browse(cr, uid, stock_prod_ids, context=context):
+                        result = i.qty
+
+                    return result
+
+        def get_mag_prod_id(self, cr, uid, ids, context=None):
+
+            mag_prod_obj = self.pool.get('magento.product.product')
+            result = {}
+            mag_prod_ids = mag_prod_obj.search(cr, uid, [('openerp_id', '=', wizard_line.product_id.id)],
+                                               context=context)
+
+            if mag_prod_ids:
+                for prod in mag_prod_obj.browse(cr, uid, mag_prod_ids, context=context):
+                    result = prod.magento_id
+
+                return result
+
+        # Do the partial delivery and open the picking that was delivered
+        # We don't need to find which view is required, stock.picking does it.
+        done = stock_picking.do_partial(
+            cr, uid, [partial.picking_id.id], partial_data, context=context)
+
+        # Update Stock in Magento
+
+        conf = self.pool.get('wyomind.config').browse(cr, uid, uid)
+        url = conf.url
+        user = conf.apiuser
+        passw = conf.apipass
+
+        # Connection
+        proxy = xmlrpclib.ServerProxy(url, allow_none=True)
+        session = proxy.login(user, passw)
+
+        print get_mag_prod_id(self, cr, uid, ids, context=context)
+        print wizard_line.product_id.id
+        print wizard_line.location_id.id
+        print wizard_line.location_dest_id.id
+        print get_stock_dest(self, cr, uid, ids, context=context)
+        print get_stock_origin(self, cr, uid, ids, context=context)
+
+        # we hardcoded the mapping local-remote warehouse
+
+        location = 0
+
+        if wizard_line.location_dest_id.id == 12:
+            location = 2
+        if wizard_line.location_dest_id.id == 15:
+            location = 4
+        if wizard_line.location_dest_id.id == 19:
+            location = 3
+
+        data_basic = {'quantity_in_stock': get_stock_dest(self, cr, uid, ids, context=context),
+                      'manage_stock': 1,
+                      'backorder_allowed': 0,
+                      'use_config_setting_for_backorders': 1}
+
+        print data_basic
+        proxy.call(session, 'advancedinventory.setData', (get_mag_prod_id(self, cr, uid, ids, context=context),
+                                                          location, data_basic))
+
+        location = 0
+        if wizard_line.location_id.id == 12:
+            location = 2
+        if wizard_line.location_id.id == 15:
+            location = 4
+        if wizard_line.location_id.id == 19:
+            location = 3
+
+        data_basic2 = {'quantity_in_stock': get_stock_origin(self, cr, uid, ids, context=context),
+                       'manage_stock': 1,
+                       'backorder_allowed': 0,
+                       'use_config_setting_for_backorders': 1}
+        print data_basic2
+        proxy.call(session, 'advancedinventory.setData', (get_mag_prod_id(self, cr, uid, ids, context=context),
+                                                          location, data_basic2))
+
+        if done[partial.picking_id.id]['delivered_picking'] == partial.picking_id.id:
+            return {'type': 'ir.actions.act_window_close'}
+
+        return {
+            'type': 'ir.actions.act_window',
+            'res_model': context.get('active_model', 'stock.picking'),
+            'name': _('Partial Delivery'),
+            'res_id': done[partial.picking_id.id]['delivered_picking'],
+            'view_type': 'form',
+            'view_mode': 'form,tree,calendar',
+            'context': context,
+        }
+
+
+stock_partial_picking()
