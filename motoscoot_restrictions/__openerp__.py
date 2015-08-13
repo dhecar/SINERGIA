1ebd11d273ae51c3c6f12f48f29740a1ca1edbb8
dhecar <dhecar@gmail.com>
2015-08-13 20:41:19 +0200

    first commit

---
+++ motoscoot_restrictions/__openerp__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    "name" : "Motoscoot restrictions",
+    "version" : "7.0",
+    "author" : "David Hernández",
+    "category" : "Generic Modules/Others",
+    "website" : "http://sinergiainformatica.net",
+    "description": "Restrictions in Mostoscoot",
+    "depends" : ['sale'],
+    "update_xml" : ['views/sale_order_view.xml'],
+    "data": ['security/security.xml', 'security/ir.model.access.csv'],
+    "active": True,
+    "installable": True
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
