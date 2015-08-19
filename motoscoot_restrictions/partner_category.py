a49db32f672090af0a4dfe2807c3ee946c151784
dhecar <dhecar@gmail.com>
2015-08-19 20:26:28 +0200

    deleted error files

---
+++ motoscoot_restrictions/partner_category.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# Limit to only one category in category_id field in pertner view
+# On change category, fill pricelist.  Taller-->Pricelist_Taller, General-->Pricelist_General
+
+
