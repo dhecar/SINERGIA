772df5d5d13e8f7f34d27ed5af8630882b96805c
dhecar <dhecar@gmail.com>
2015-08-27 12:16:55 +0200

    Update description

--- sine_pricelist/__openerp__.py
+++ sine_pricelist/__openerp__.py
@@ -1,13 +1,15 @@
 # -*- coding: utf-8 -*-
 {
-    "name" : "Tarifas de precio asociadas a la marca del producto",
-    "version" : "7.0",
-    "author" : "David Hernández",
-    "category" : "Generic Modules/Others",
-    "website" : "http://sinergiainformatica.net",
-    "description": "Extiende el creador de tarifas para soportar la aplicacion de la misma a la marca del producto. Requiere de product_brand",
-    "depends" : ["product", "product_brand", "base"],
-    "update_xml" : ["pricelist_view.xml"],
+    "name": "Tarifas de precio asociadas a la marca del producto",
+    "version": "7.0",
+    "author": "David Hernández",
+    "category": "Generic Modules/Others",
+    "website": "http://sinergiainformatica.net",
+    "description": '''* Extiende el creador de tarifas para soportar la aplicacion de la misma
+                      a la marca del producto. Requiere de product_brand.
+                      * Añade relación entre usuario y tarifa''',
+    "depends": ["product", "product_brand", "base"],
+    "update_xml": ["pricelist_view.xml"],
     "active": True,
     "installable": True
 }
