b348d6f121dd42f76bc92091559f3d79c6af2336
cubells <vicent@vcubells.net>
2015-08-27 16:13:57 +0200

    [IMP] Millores diverses

--- product_pricelist_improved/wizard/calculate_pricelist.py
+++ product_pricelist_improved/wizard/calculate_pricelist.py
@@ -22,7 +22,6 @@
 ##############################################################################
 
 from openerp.osv import osv, fields, orm
-from openerp.tools.translate import _
 import logging
 _logger = logging.getLogger(__name__)
 
@@ -38,7 +37,6 @@ class CalculatePricelist(orm.TransientModel):
             ('initial', 'Initial'),
             ('done', 'Done'),
         ], 'State', readonly=True),
-        'partner_id': fields.many2one('res.partner', 'Customer', select=True),
         'product_id': fields.many2one('product.product', 'Product',
                                       domain=[('sale_ok', '=', True)],
                                       required=True),
@@ -64,6 +62,41 @@ class CalculatePricelist(orm.TransientModel):
         return res
 
     def button_calculate(self, cr, uid, ids, context=None):
+
+        def get_real_price(res_dict, product_id, qty, pricelist):
+            pricelist_obj = self.pool['product.pricelist']
+            item_obj = self.pool['product.pricelist.item']
+            price_type_obj = self.pool['product.price.type']
+            product_obj = self.pool.get('product.product')
+            field_name = 'list_price'
+            item = res_dict.get('item_id', False) and res_dict['item_id'].get(
+                pricelist, False)
+            if item:
+                if item_obj.read(cr, uid, [item], ['base']):
+                    item_base = item_obj.read(
+                        cr, uid, [item], ['base'])[0]['base']
+                    if item_base > 0:
+                        field_name = price_type_obj.browse(
+                            cr, uid, item_base).field
+                else:
+                    pricelist = pricelist_obj.browse(cr, uid, pricelist)
+                    version = pricelist.version_id and \
+                              pricelist.version_id[0].id or False
+                    if version:
+                        item = item_obj.search(cr, uid, [
+                            ('price_version_id', '=', version)
+                        ])
+                        if item:
+                            item_base = item_obj.read(
+                                cr, uid, item, ['base'])[0]['base']
+                            if item_base > 0:
+                                field_name = price_type_obj.browse(
+                                    cr, uid, item_base).field
+            product = product_obj.browse(cr, uid, product_id, context)
+            product_read = product_obj.read(
+                cr, uid, product_id, [field_name], context=context)
+            return product_read[field_name]
+
         if context is None:
             context = {}
         self.write(cr, uid, ids, {'state': 'done'})
@@ -77,21 +110,43 @@ class CalculatePricelist(orm.TransientModel):
         price_ids = pricelist_obj.search(cr, uid, [('type', '=', 'sale')],
                                          context=context)
         for plist in pricelist_obj.browse(cr, uid, price_ids, context=context):
+            tax = data.product_id.taxes_id and \
+                data.product_id.taxes_id[0].amount or 0.0
+            tax += 1
+            if plist.version_id:
+                if plist.version_id[0].items_id:
+                    print plist.version_id[0].items_id[0].base
             price = pricelist_obj.price_get(
-                cr, uid, [plist.id], data.product_id.id, data.qty,
-                data.partner_id and data.partner_id.id or False,
+                cr, uid, [plist.id], data.product_id.id, data.qty, False,
                 context=context)[plist.id]
+            list_price = data.product_id.list_price or 0.0
+            percent = 0.0
+            if list_price:
+                percent = 100 - ((price * 100) / list_price)
+            base = get_real_price(
+                pricelist_obj.price_get(
+                    cr, uid, [plist.id], data.product_id.id, data.qty, False,
+                    context=context),
+                data.product_id.id, float(data.qty), plist.id)
             standard_price = data.product_id.standard_price or 0.0
+            default_code = data.product_id.default_code or ''
+            if default_code:
+                default_code = '[%s]' % default_code
+
             values = {
                 'wizard_id': ids[0],
                 'pricelist_id': plist.id,
                 'product_id': data.product_id.id,
+                'default_code': default_code,
                 'qty': float(data.qty),
+                'base': base,
                 'price': price,
                 'standard_price': standard_price,
                 'margin': price - standard_price,
+                'percent': percent,
+                'total': price * tax,
             }
-            line_obj.create(cr, uid, values,context=context)
+            line_obj.create(cr, uid, values, context=context)
 
         return {
             'type': 'ir.actions.act_window',
@@ -114,9 +169,13 @@ class CalculatePricelistLine(orm.TransientModel):
                                         readonly=True),
         'product_id': fields.many2one('product.product', 'Product',
                                       readonly=True),
+        'default_code': fields.char('Product', size=64, readonly=True),
         'qty': fields.integer('Quantity', readonly=True),
-        'price': fields.float('Price', readonly=True),
+        'base': fields.float('Base Price', readonly=True),
+        'price': fields.float('Final Price', readonly=True),
         'standard_price': fields.float('Standard Price', readonly=True),
         'margin': fields.float('Margin', readonly=True),
+        'percent': fields.float('Discount', readonly=True),
+        'total': fields.float('Price with Tax', readonly=True),
     }
 
