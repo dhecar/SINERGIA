4dc7fdb10fe9589b475686aa617cddcfc93f5428
dhecar <dhecar@gmail.com>
2015-09-04 17:06:10 +0200

    [IMP] Fields required + help information

--- delivery_carrier_file_ups/generator/ups_generator.py
+++ delivery_carrier_file_ups/generator/ups_generator.py
@@ -93,7 +93,7 @@ class UpsFileGenerator(CarrierFileGenerator):
         line.goods = configuration.ups_description_goods
         line.reference1 = picking.name
         line.reference2 = picking.name
-        line.weight = "%.2f" % (picking.weight).replace('.', ',')
+        line.weight = ("%.2f" % (picking.weight)).replace('.', ',')
         line.cash = configuration.ups_cash
         line.ups_cod_price = configuration.ups_cod_price
         line.total = picking.sale_id.amount_total.replace('.', ',')
