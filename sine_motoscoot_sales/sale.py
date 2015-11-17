7dafa7f42eff1afd407b4d2610e65da5fc654a7c
dhecar <dhecar@gmail.com>
2015-11-17 16:24:30 +0100

    partner alert in sale form

--- sine_motoscoot_sales/sale.py
+++ sine_motoscoot_sales/sale.py
@@ -1,6 +1,6 @@
 ##############################################################################
 #
-#    OpenERP, Open Source Management Solution
+# OpenERP, Open Source Management Solution
 #    Copyright (C) 2004-2009 Tiny SPRL (<http://tiny.be>).
 #
 #    This program is free software: you can redistribute it and/or modify
@@ -24,27 +24,31 @@ from osv import fields, osv
 class sale_order_line(osv.osv):
     _inherit = 'sale.order.line'
     _columns = {
-        'stock_grn': fields.related('product_id',  'stock_grn', type='float', string='G'),
-        'stock_bcn': fields.related('product_id', 'stock_bcn', type='float',  string='B'),
-        'stock_pt': fields.related('product_id', 'stock_pt', type='float',  string='P'),
-        'date_ordered': fields.related('order_id', 'date_order', type='char', relation='sale.order', string='Fecha Orden'),
+        'stock_grn': fields.related('product_id', 'stock_grn', type='float', string='G'),
+        'stock_bcn': fields.related('product_id', 'stock_bcn', type='float', string='B'),
+        'stock_pt': fields.related('product_id', 'stock_pt', type='float', string='P'),
+        'date_ordered': fields.related('order_id', 'date_order', type='char', relation='sale.order',
+                                       string='Fecha Orden'),
 
     }
+
+
 sale_order_line()
 
 
 class sale_order(osv.osv):
-
     _inherit = 'sale.order'
     _columns = {
-        'sale_internal_comment':  fields.text('Internal Comment', help=''),
+        'sale_internal_comment': fields.text('Internal Comment', help=''),
         'picking_status': fields.related('picking_ids', 'state', type='char', string="Estado envio"),
         'date_send': fields.related('picking_ids', 'date_done', type='char', string="Fecha Envio"),
         'invoice_status': fields.related('invoiced', type='boolean', string="Estado Factura"),
         'traking': fields.related('picking_ids', 'carrier_tracking_ref', type='char', string="Tracking"),
-
+        'alert': fields.related('partner_id', 'sale_warn_msg', type='char', help='Sale Alert', string='Alerta'),
 
     }
+
+
 sale_order()
 
 
