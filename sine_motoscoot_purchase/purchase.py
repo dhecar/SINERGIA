fb5d0c7d4347bd527b7000ae17bf1f157f119abd
dhecar <dhecar@gmail.com>
2015-11-28 15:08:21 +0100

    IN/OUT

--- sine_motoscoot_purchase/purchase.py
+++ sine_motoscoot_purchase/purchase.py
@@ -27,6 +27,8 @@ class purchase_order_line(osv.osv):
         'stock_grn': fields.related('product_id','stock_grn', type='float', string='G'),
         'stock_bcn': fields.related('product_id','stock_bcn', type='float',  string='B'),
         'stock_pt': fields.related('product_id','stock_pt', type='float',  string='P'),
+        'incoming': fields.related('product_id', 'incoming_qty', type='float', string='IN'),
+        'outgoing': fields.related('product_id', 'outgoing_qty', type='float', string='OUT'),
 
     }
 
