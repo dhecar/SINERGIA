b686bd9f4de4f7c09d4f28a30e65985c162c81a1
dhecar <dhecar@gmail.com>
2016-05-13 17:11:28 +0200

    improved

--- sine_wyomind_stock_update/wyomind_update.py
+++ sine_wyomind_stock_update/wyomind_update.py
@@ -127,9 +127,9 @@ class stock_change_product_qty(osv.osv_memory):
                           }
 
             proxy.call(session, 'advancedinventory.setData', (get_mag_prod_id(self, cr, uid, ids, context=context),
-                                                              location, data_basic))
+                                                              location, data_basic)),
 
-            proxy.call(session, 'advancedinventory.setMultistock', (get_mag_prod_id(self, cr, uid, ids, context=context), True))
+            proxy.call(session, 'advancedinventory.setMultistock', (21637, True))
 
 
         return {}
