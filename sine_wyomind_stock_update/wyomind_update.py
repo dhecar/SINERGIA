cc3dfc3e7743690449e5c9ba9383a4b8d451a43f
dhecar <dhecar@gmail.com>
2016-06-01 14:07:57 +0200

    improved for partial moves in purchases

--- sine_wyomind_stock_update/wyomind_update.py
+++ sine_wyomind_stock_update/wyomind_update.py
@@ -318,6 +318,167 @@ class stock_partial_picking(osv.osv_memory):
 stock_partial_picking()
 
 
+class stock_partial_move(osv.osv_memory):
+    _inherit = "stock.partial.move"
+
+    def do_partial(self, cr, uid, ids, context=None):
+        res = super(stock_partial_move, self).do_partial(cr, uid, ids, context=context)
+        partial = self.browse(cr, uid, ids[0], context=context)
+        # REMOTE
+        db_obj = self.pool['base.external.dbsource']
+
+        # Wyomind Config
+        conf_obj = self.pool.get('wyomind.config')
+        conf_ids = conf_obj.search(cr, uid, [('id', '=', 1)])
+        for x in conf_obj.browse(cr, uid, conf_ids):
+            url = x.url
+            user = x.apiuser
+            passw = x.apipass
+
+            # Connection
+            proxy = xmlrpclib.ServerProxy(url, allow_none=True)
+            session = proxy.login(user, passw)
+
+        # Wyomind stock update
+
+        for wizard_line in partial.move_ids:
+
+            """Locations hardcoded"""
+            # TODO put locations in config file
+
+            location2 = 0
+            if wizard_line.location_dest_id.id == 12:
+                location2 = 2
+            if wizard_line.location_dest_id.id == 15:
+                location2 = 4
+            if wizard_line.location_dest_id.id == 19:
+                location2 = 3
+
+            """ Update dest stock location for partial in movements"""
+            # If product is linked to magento
+            if wizard_line.product_id.magento_bind_ids:
+                # product stock
+                cr.execute("""SELECT qty FROM stock_report_prodlots WHERE
+                        location_id =%s AND product_id = %s""" %
+                           (wizard_line.location_dest_id.id, wizard_line.product_id.id))
+                qty = cr.fetchone()[0]
+
+                # CASE GRN
+                if wizard_line.location_dest_id.id == 12:
+                    ads = db_obj.get_stock(cr, SUPERUSER_ID, ids, wizard_line.product_id.id,
+                                           wizard_line.location_dest_id.id, context=context)
+                    q = qty - ads
+                else:
+                    q = qty
+
+                # magento id
+                cr.execute('SELECT magento_id'
+                           ' FROM magento_product_product'
+                           ' WHERE openerp_id =%s' % wizard_line.product_id.id)
+                mag_id = cr.fetchone()[0]
+
+                data_basic = {'quantity_in_stock': q,
+                              'manage_stock': 1,
+                              'backorder_allowed': 0,
+                              'use_config_setting_for_backorders': 0
+                              }
+
+                proxy.call(session, 'advancedinventory.setData',
+                           (mag_id, location2, data_basic))
+
+        return res
+
+
+class stock_move(osv.osv):
+    _inherit = 'stock.move'
+
+    def action_done(self, cr, uid, ids, context=None):
+        res = super(stock_move, self).action_done(cr, uid, ids, context=context)
+        # REMOTE
+        db_obj = self.pool['base.external.dbsource']
+
+        # Wyomind Config
+        conf_obj = self.pool.get('wyomind.config')
+        conf_ids = conf_obj.search(cr, uid, [('id', '=', 1)])
+        for x in conf_obj.browse(cr, uid, conf_ids):
+            url = x.url
+            user = x.apiuser
+            passw = x.apipass
+
+            # Connection
+            proxy = xmlrpclib.ServerProxy(url, allow_none=True)
+            session = proxy.login(user, passw)
+
+        picking_ids = []
+        move_ids = []
+        wf_service = netsvc.LocalService("workflow")
+        if context is None:
+            context = {}
+
+        todo = []
+        for move in self.browse(cr, uid, ids, context=context):
+            if move.state == "draft":
+                todo.append(move.id)
+        if todo:
+            self.action_confirm(cr, uid, todo, context=context)
+
+        for move in self.browse(cr, uid, ids, context=context):
+            if move.state != 'done':
+                continue
+            move_ids.append(move.id)
+
+            if move.picking_id:
+                picking_ids.append(move.picking_id.id)
+
+            # Wyomind stock update
+            for wizard_line in move_ids:
+
+                """Locations hardcoded"""
+                # TODO put locations in config file
+
+                location2 = 0
+                if move.location_dest_id.id == 12:
+                    location2 = 2
+                if move.location_dest_id.id == 15:
+                    location2 = 4
+                if move.location_dest_id.id == 19:
+                    location2 = 3
+
+                """ Update dest stock location for partial in movements"""
+                # If product is linked to magento
+                if move.product_id.magento_bind_ids:
+                    # product stock
+                    cr.execute("""SELECT qty FROM stock_report_prodlots WHERE
+                                    location_id =%s AND product_id = %s""" %
+                               (move.location_dest_id.id, move.product_id.id))
+                    qty = cr.fetchone()[0]
+
+                    # CASE GRN
+                    if move.location_dest_id.id == 12:
+                        ads = db_obj.get_stock(cr, SUPERUSER_ID, ids, move.product_id.id,
+                                               move.location_dest_id.id, context=context)
+                        q = qty - ads
+                    else:
+                        q = qty
+
+                    # magento id
+                    cr.execute('SELECT magento_id'
+                               ' FROM magento_product_product'
+                               ' WHERE openerp_id =%s' % move.product_id.id)
+                    mag_id = cr.fetchone()[0]
+
+                    data_basic = {'quantity_in_stock': q,
+                                  'manage_stock': 1,
+                                  'backorder_allowed': 0,
+                                  'use_config_setting_for_backorders': 0
+                                  }
+
+                    proxy.call(session, 'advancedinventory.setData',
+                               (mag_id, location2, data_basic))
+
+        return res
+
+
 class InvoiceSaleOrder(osv.TransientModel):
     _inherit = 'sale.order.invoiced'
 
@@ -373,5 +534,6 @@ class InvoiceSaleOrder(osv.TransientModel):
                             proxy.call(session, 'advancedinventory.setData',
                                        (mag_id, 2, data_basic))
         return res
+
 
 InvoiceSaleOrder()
