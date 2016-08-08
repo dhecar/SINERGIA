7687f8251f5bda66be8e8bea70261f64441a755c
dhecar <dhecar@gmail.com>
2016-08-08 11:47:58 +0200

    [IMp]Addes transitional locations for internal movements.

--- sine_wyomind_stock_update/wyomind_update.py
+++ sine_wyomind_stock_update/wyomind_update.py
@@ -114,22 +114,10 @@ class stock_move(osv.osv):
     _inherit = 'stock.move'
 
     def action_done(self, cr, uid, ids, context=None):
-        tiempo_inicial = time.time()
+
         res = super(stock_move, self).action_done(cr, uid, ids, context=context)
         # REMOTE
         db_obj = self.pool['base.external.dbsource']
-
-        # Wyomind Config
-        conf_obj = self.pool.get('wyomind.config')
-        conf_ids = conf_obj.search(cr, uid, [('id', '=', 1)])
-        for x in conf_obj.browse(cr, uid, conf_ids):
-            url = x.url
-            user = x.apiuser
-            passw = x.apipass
-
-            # Connection
-            proxy = xmlrpclib.ServerProxy(url, allow_none=True)
-            session = proxy.login(user, passw)
 
         picking_ids = []
         move_ids = []
@@ -152,10 +140,22 @@ class stock_move(osv.osv):
             if move.picking_id:
                 picking_ids.append(move.picking_id.id)
 
-                # Wyomind stock update
+        # Wyomind Config
+        conf_obj = self.pool.get('wyomind.config')
+        conf_ids = conf_obj.search(cr, uid, [('id', '=', 1)])
+        for x in conf_obj.browse(cr, uid, conf_ids):
+            url = x.url
+            user = x.apiuser
+            passw = x.apipass
+
+            # Connection
+
+            proxy = xmlrpclib.ServerProxy(url, allow_none=True)
+            session = proxy.login(user, passw)
+
         for move in self.browse(cr, uid, ids, context=context):
 
-            """Locations hardcoded"""
+            """ Locations hardcoded"""
             # TODO put locations in config file
 
             location = 0
@@ -176,10 +176,11 @@ class stock_move(osv.osv):
 
             """ Update dest stock location for partial in movements"""
             # If product is linked to magento
-            if move.product_id.magento_bind_ids and location2 != 0 and move.picking_id:
+            if move.product_id.magento_bind_ids and move.location_dest_id.id not in (0, 22, 24, 25) and move.picking_id:
+
                 # product stock
                 cr.execute("""SELECT qty FROM stock_report_prodlots WHERE
-                                    location_id =%s AND product_id = %s""" %
+                                                location_id =%s AND product_id = %s""" %
                            (move.location_dest_id.id, move.product_id.id))
                 qty = cr.fetchone()[0]
 
@@ -208,10 +209,11 @@ class stock_move(osv.osv):
 
             """ Update origin stock location for partial out movements"""
             # If product is linked to magento
-            if move.product_id.magento_bind_ids and location != 0 and move.picking_id:
+            if move.product_id.magento_bind_ids and move.location_id.id not in (0, 22, 24, 25) and move.picking_id:
+
                 # product stock
                 cr.execute("""SELECT qty FROM stock_report_prodlots WHERE
-                                location_id =%s AND product_id = %s""" %
+                                            location_id =%s AND product_id = %s""" %
                            (move.location_id.id, move.product_id.id))
                 qty = cr.fetchone()[0]
 
@@ -237,10 +239,6 @@ class stock_move(osv.osv):
 
                 proxy.call(session, 'advancedinventory.setData',
                            (mag_id, location, data_basic))
-
-            tiempo_final = time.time()
-            tiempo_ejecucion = tiempo_final - tiempo_inicial
-            print ' stock_move', tiempo_ejecucion
 
         return res
 
