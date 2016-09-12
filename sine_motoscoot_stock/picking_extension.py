c63c7d4d7dcc0340611e92db7a26e8b295ffebbc
dhecar <dhecar@gmail.com>
2016-09-12 10:04:05 +0200

    improved

--- sine_motoscoot_stock/picking_extension.py
+++ sine_motoscoot_stock/picking_extension.py
@@ -1,4 +1,4 @@
-from osv import fields, osv
+from openerp.osv import fields, osv
 import one2many_sorted
 from zebra import zebra
 from time import sleep
@@ -104,89 +104,107 @@ class stock_move(osv.osv):
     }
 
 
-### Redefine the class to get fields from in moviments
-
 
 class stock_partial_picking(osv.osv):
     _inherit = "stock.partial.picking"
 
-    # Get default queue name
-    def get_queue(self, cr, uid, field_names=None, arg=None, context=None):
+    # Get default label printer for current user
+    def get_default_label_printer(self, cr, uid, field_names=None, arg=None, context=None):
         result = {}
-        pool_obj = self.pool.get('qz.config')
-        pool_ids = pool_obj.search(cr, uid, [('qz_default', '=', 1)])
+        pool_obj = self.pool.get('res.users')
+        pool_ids = pool_obj.search(cr, uid, [('id', '=', uid)])
         if pool_ids:
             for i in pool_obj.browse(cr, uid, pool_ids, context=context):
-                result = i.qz_printer.system_name
+                result = i.epl_printer_id.system_name
             return result
+        else:
+
+            msg = "The current user don't have any printer configured. Please contact the master of pupets"
+            raise osv.except_osv(_('Warning !'), _(msg))
 
     # Prepare EPL data (escaped)
 
     def prepare_epl_data(self, cr, uid, ids, context=None):
-        # Impresora
+        # Get default printer for current user
+        printer = self.get_default_label_printer(cr, uid, ids, context=context)
+        # Default configuration for selected printer
         pool_obj = self.pool.get('qz.config')
-        pool_ids = pool_obj.search(cr, uid, [('qz_default', '=', 1)])
+        pool_ids = pool_obj.search(cr, uid, [('qz_printer.system_name', '=', printer)])
         # Pickings
         partial_pick_obj = self.pool.get('stock.partial.picking.line')
         line_ids = context.get('line_id', []) or []
-        partial_ids = partial_pick_obj.search(cr, uid, [('wizard_id', '=', ids), ('id', '=', line_ids)])
-        # For
+        # Ordenamos la lineas
+        line_ids_sorted = sorted(line_ids)
+        partial_ids = partial_pick_obj.search(cr, uid, [('wizard_id', '=', ids), ('id', '=', line_ids_sorted)])
+
         for line in partial_pick_obj.browse(cr, uid, partial_ids, context=context):
-
-            # Limit size:
-
-            if len(line.product_id.name_template) > 40:
-                product_name = line.product_id.name_template[:40] + '..'
-            else:
-                product_name = line.product_id.name_template
 
             if pool_ids:
                 for i in pool_obj.browse(cr, uid, pool_ids, context=context):
-                    for fields in i.qz_field_ids:
+                    # Model from I get info
+                    model = i.model_id.model
+                    partial_result = []
 
-                        # Format: Bp1,p2,p3,p4,p5,p6,p7,p8,"DATA"\n
+                    for fields in i.qz_field_ids:
+                        # Fields name to search in model
+                        name_field = fields.qz_field_id.name
+                        # Get fields from Model
+                        printing_field = self.pool.get(model).read(cr, uid, partial_ids, [name_field], context=context)
+                        # Limit to 40 characters on long lines
+
+                        for x in printing_field:
+                            if x[name_field]:
+                                print_field = x[name_field]
+                                if len(print_field) > 40:
+                                    print_field = print_field[:40] + '..'
+                                else:
+                                    print_field = print_field
+                                # Barcode Format: Bp1,p2,p3,p4,p5,p6,p7,p8,"DATA"\n
 
                         if fields.qz_field_type == 'barcode':
-
                             data = []
                             data += {'B' + str(fields.h_start_p1) + ',' +
-                                     str(fields.v_start_p2) + ',' +
-                                     str(fields.rotation_p3) + ',' +
-                                     str(fields.bar_sel_p4) + ',' +
-                                     str(fields.n_bar_w_p5) + ',' +
-                                     str(fields.w_bar_w_p6) + ',' +
-                                     str(fields.bar_height_p7) + ',' +
-                                     str(fields.human_read_p8) + ',' + '"' +
-                                     str(line.product_id.default_code) + '"' + '\n'}
-                            # TODO get value to print from qz.config
+                                             str(fields.v_start_p2) + ',' +
+                                             str(fields.rotation_p3) + ',' +
+                                             str(fields.bar_sel_p4) + ',' +
+                                             str(fields.n_bar_w_p5) + ',' +
+                                             str(fields.w_bar_w_p6) + ',' +
+                                             str(fields.bar_height_p7) + ',' +
+                                             str(fields.human_read_p8) + ',' + '"' +
+                                             str(print_field) + '"' + '\n'}
 
-                            # text field Format: Ap1,p2,p3,p4,p5,p6,p7,"DATA"\n
 
+                        # Text field Format: Ap1,p2,p3,p4,p5,p6,p7,"DATA"\n
                         else:
 
-                            data2 = []
-                            data2 += {'A' + str(fields.h_start_p1) + ',' +
-                                      str(fields.v_start_p2) + ',' +
-                                      str(fields.rotation_p3) + ',' +
-                                      str(fields.font_p4) + ',' +
-                                      str(fields.h_multiplier_p5) + ',' +
-                                      str(fields.v_multiplier_p6) + ',' +
-                                      str(fields.n_r_p7) + ',' +
-                                      unicodedata.normalize('NFKD', product_name).encode('ascii',
-                                                                                         'ignore') + '"' + '\n'}
+                            data = []
+                            data += {'A' + str(fields.h_start_p1) + ',' +
+                                             str(fields.v_start_p2) + ',' +
+                                             str(fields.rotation_p3) + ',' +
+                                             str(fields.font_p4) + ',' +
+                                             str(fields.h_multiplier_p5) + ',' +
+                                             str(fields.v_multiplier_p6) + ',' +
+                                             str(fields.n_r_p7) + ',' + '"' +
+                                             unicodedata.normalize('NFKD', print_field).encode('ascii',
+                                                                                               'ignore') + '"' + '\n'}
 
-                            # TODO get value to print from qz.config
-                """
-                    Example of ELP commands to send
-                    N
-                    A40,80,0,4,1,1,N,"Tangerine Duck 4.4%"
-                    A40,198,0,3,1,1,N,"Duty paid on 39.9l"
-                    A40,240,0,3,1,1,N,"Gyle: 127     Best Before: 16/09/2011"
-                    A40,320,0,4,1,1,N,"Pump & Truncheon"
-                    P1
-                 """
+                            """
+                            Example of ELP commands to send
+                            N
+                            A40,80,0,4,1,1,N,"Tangerine Duck 4.4%"
+                            A40,198,0,3,1,1,N,"Duty paid on 39.9l"
+                            A40,240,0,3,1,1,N,"Gyle: 127     Best Before: 16/09/2011"
+                            A40,320,0,4,1,1,N,"Pump & Truncheon"
+                            P1
+                            """
+                            # Partial result that create one line for each field to print
 
-            result = '"""\n' + 'N\n' + ''.join(data) + ''.join(data2) + 'P1\n"""'
+                        partial_result += data
+
+                    strings = '\n'.join(partial_result)
+                    result = '"""\n' + 'N\n'
+                    result += strings
+                    result += 'P1\n"""'
 
             return result
 
@@ -223,7 +241,9 @@ class stock_partial_picking(osv.osv):
         # record_ids = context and context.get('active_ids', []) or []
         partial = self.browse(cr, uid, ids[0], context=context)
         # Select Printable Labels  products
-        for wizard_line in partial.move_ids:
+        # sort by sku
+        partial_sorted = sorted(partial, key=partial.default_code)
+        for wizard_line in partial_sorted.move_ids:
             # Update the context with each line value in each cicle.
             context.update({'line_id': wizard_line.id})
             # Quantity must be Positive
